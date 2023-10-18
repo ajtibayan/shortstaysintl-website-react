d0d40c14eab83a0adbfea799684f43c98ee6082b
ajtibayan <ajtibayan.freelance@gmail.com>
2023-10-17 22:54:30 -0400

    Setup Typescript in project

---
+++ src/components/home/LookingFor.tsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+
+// Components and Styles
+import './LookingFor.css';
+
+// Assets;
+import { lookingForContent as lfcData } from '../../data/LookingForSection';
+
+type LookingForData = {
+  faIcon: string;
+  heading: string;
+  text: string;
+  id: number;
+};
+
+const LookingFor = () => {
+  const [lookingForContent, setLookingForContent] = useState(lfcData);
+
+  return (
+    <section className="section-wrapper section-lookingFor">
+      <div className="section-container">
+        {lookingForContent.map(
+          ({ faIcon, heading, text, id }: LookingForData) => {
+            return (
+              <div className="bucket-container" key={id}>
+                <div className="box-container">
+                  <div className="box-wrapper">
+                    <div className="box-wrapper_icon-container">
+                      <i className={faIcon}></i>
+                    </div>
+                    <div className="box-wrapper_text-container">
+                      <h3 className="mb-4 text-2xl font-bold text-[#282f44]">
+                        {heading}
+                      </h3>
+                      <p className="text-[#717580]">{text}</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            );
+          },
+        )}
+      </div>
+    </section>
+  );
+};
+
+export default LookingFor;
